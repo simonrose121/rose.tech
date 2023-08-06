f1bddf0d063e0896d286ab137fa0769638068318
Simon Rose <simon.rose@542.digital>
2023-08-06 17:30:01 +0100

    feat: added hero

--- app/header.tsx
+++ app/header.tsx
@@ -16,9 +16,9 @@ export default function Header() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<header className="bg-white">
+		<header className="bg-grey-900 absolute inset-x-0 top-0 z-50">
 			<nav
-				className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+				className="flex items-center justify-between p-6 lg:px-8"
 				aria-label="Global"
 			>
 				<div className="flex lg:flex-1">
@@ -30,7 +30,7 @@ export default function Header() {
 				<div className="flex lg:hidden">
 					<button
 						type="button"
-						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
 						onClick={() => setMobileMenuOpen(true)}
 					>
 						<span className="sr-only">Open main menu</span>
@@ -42,7 +42,7 @@ export default function Header() {
 						<a
 							key={item.name}
 							href={item.href}
-							className="text-sm font-semibold leading-6 text-gray-900"
+							className="text-sm font-semibold leading-6 text-white"
 						>
 							{item.name}
 						</a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
+import Hero from './hero';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main>
+			<Hero></Hero>
+		</main>
 	);
 }
