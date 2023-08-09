fdcd584a8488624602c287145bed70310c45c0b9
Simon Rose <simon.rose@542.digital>
2023-08-09 10:21:47 +0100

    feat: clients section

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,50 +1,35 @@
 export default function Hero() {
 	return (
-		<div className="bg-white">
-			<div className="isolate px-6 pt-14 lg:px-8">
-				<img
-					src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80&auto=format&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-					alt=""
-					className="absolute inset-0 -z-10 h-full w-full object-cover"
-				/>
-				<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-					<div className="text-center">
-						<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-							Software projects
-							<br />
-							<span className="text-primary-500">made easy.</span>
-						</h1>
-						<p className="mt-6 text-lg leading-8 text-white">
-							We enable and drive businesses towards software
-							success.
-						</p>
-						<div className="mt-10 flex items-center justify-center gap-x-6">
-							<a
-								href="#"
-								className="rounded-md bg-primary-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-							>
-								Get started
-							</a>
-							<a
-								href="#"
-								className="text-sm font-semibold leading-6 text-white"
-							>
-								Learn more <span aria-hidden="true">→</span>
-							</a>
-						</div>
+		<div className="relative isolate px-6 pt-14 lg:px-8">
+			<img
+				src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80&auto=format&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+				alt=""
+				className="absolute inset-0 -z-10 h-full w-full object-cover"
+			/>
+			<div className="mx-auto min-h-full max-w-2xl py-32 sm:py-48 lg:py-56">
+				<div className="text-center">
+					<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+						Software projects
+						<br />
+						<span className="text-primary-500">made easy.</span>
+					</h1>
+					<p className="mt-6 text-lg leading-8 text-white">
+						We enable and drive businesses towards software success.
+					</p>
+					<div className="mt-10 flex items-center justify-center gap-x-6">
+						<a
+							href="#"
+							className="rounded-md bg-primary-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>
+							Get started
+						</a>
+						<a
+							href="#"
+							className="text-sm font-semibold leading-6 text-white"
+						>
+							Learn more <span aria-hidden="true">→</span>
+						</a>
 					</div>
-				</div>
-				<div
-					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-					aria-hidden="true"
-				>
-					<div
-						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-						style={{
-							clipPath:
-								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-						}}
-					/>
 				</div>
 			</div>
 		</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Hero from './hero';
+import Clients from './clients';
 
 export default function Home() {
 	return (
 		<main>
 			<Hero></Hero>
+			<Clients></Clients>
 		</main>
 	);
 }
